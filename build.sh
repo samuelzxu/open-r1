5cc7e1e4784ac19e34e645e951af56ace835ebb7
Samuel Xu <samuelzxu@gmail.com>
2025-03-29 15:04:11 +0000

    Updates

---
+++ build.sh
@@ -0,0 +1,10 @@
+pip install uv
+rm -rf openr1
+uv venv openr1 --python 3.11 && source openr1/bin/activate && uv pip install --upgrade pip --link-mode=copy
+uv pip install vllm==0.7.1 --link-mode=copy
+uv pip install setuptools --link-mode=copy
+uv pip install flash-attn --no-build-isolation --link-mode=copy
+GIT_LFS_SKIP_SMUDGE=1 uv pip install -e ".[dev]" --link-mode=copy --no-build-isolation
+uv pip install wandb
+wandb login
+huggingface-cli login
